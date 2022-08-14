4166aa16e540b07a521aa3acd3ada19112200b48
bryanjosem79 <bryanjosem79@gmail.com>
2022-08-14 17:59:48 -0400

    Boton NO javascript listo

--- index.js
+++ index.js
@@ -2,4 +2,14 @@ const siBtn = document.querySelector('#siBtn');
 
 siBtn.addEventListener('click',function() {
     alert('Sabia que ibas a decir que SI :D');
-})
+});
+
+const noBtn = document.querySelector('#noBtn');
+
+noBtn.addEventListener('mouseover',function() {
+    const randomX = parseInt(Math.random()+100);
+    const randomY = parseInt(Math.random()*100);
+    noBtn.style.setProperty('top',randomY+'%');
+    noBtn.style.setProperty('left',randomX+'%');
+    noBtn.style.setProperty('transform',`translate(-${randomX}%,-${randomY}%)`);
+});
